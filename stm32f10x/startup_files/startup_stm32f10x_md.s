d7d8b0bd4411c499ba4e97c132822f49db9e9657
Vladimir Kanavkin <vkanavkin@bk.ru>
2019-02-10 23:40:25 +0100

    corrected startup file

--- stm32f10x/startup_files/startup_stm32f10x_md.s
+++ stm32f10x/startup_files/startup_stm32f10x_md.s
@@ -93,8 +93,8 @@ LoopFillZerobss:
 
 /* Call the clock system intitialization function.*/
     bl  SystemInit
-/* Call static constructors */
-    bl __libc_init_array
+/* Do not call static constructors 
+    bl __libc_init_array */
 /* Call the application's entry point.*/
 	bl	main
 	bx	lr
